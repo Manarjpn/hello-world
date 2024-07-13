8903f76fa835feea21d3facf3210674d0a614270
Behloul Manar <64891191+Manarjpn@users.noreply.github.com>
2024-07-13 13:00:43 +0900

    Update Breast Cancer Wisconsin.py

--- Breast Cancer Wisconsin.py
+++ Breast Cancer Wisconsin.py
@@ -10,5 +10,4 @@ y = dataset.iloc[:, -1].values
 
 #データセットをトレーニングセットとテストセットに分割する
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
